79dbf629b9c2ed2cd5b0e1e2f55dde19c961793e
Tom9603 <tom.ochietti@gmail.com>
2025-06-30 12:19:20 +0000

    exo

--- managers/CategoryManager.php
+++ managers/CategoryManager.php
@@ -4,8 +4,38 @@
  * @link : https://github.com/Gaellan
  */
 
-
 class CategoryManager extends AbstractManager
 {
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function findAll(): array {
+        $query = $this->db->prepare('SELECT * FROM categories');
+        $query->execute();
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        $categories = [];
+        
+        foreach ($result as $item) {
+            $category = new Category($item["title"], $item["description"]);
+            $category->setId((int)$item["id"]);
+            $categories[] = $category;
+        }
+        
+        return $categories;
+    }
+    
+    public function findOne(int $id): ?Category {
+        $query = $this->db->prepare('SELECT * FROM categories WHERE id = :id');
+        $query->execute(['id' => $id]);
+        $item = $query->fetch(PDO::FETCH_ASSOC);
 
+        if ($item) {
+            $category = new Category($item["title"], $item["description"]);
+            $category->setId((int)$item["id"]);
+            return $category;
+        }
+
+        return null;
+    }
 }
--- managers/PostManager.php
+++ managers/PostManager.php
@@ -4,8 +4,79 @@
  * @link : https://github.com/Gaellan
  */
 
-
 class PostManager extends AbstractManager
 {
+    private UserManager $userManager;
 
+    public function __construct() {
+        parent::__construct();
+        $this->userManager = new UserManager(); // pour récupérer les objets User
+    }
+
+    public function findLatest(): array {
+        $query = $this->db->prepare('SELECT * FROM posts ORDER BY created_at DESC LIMIT 4');
+        $query->execute();
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        $posts = [];
+
+        foreach ($result as $item) {
+            $author = $this->userManager->findOne((int)$item["author_id"]);
+            $post = new Post(
+                $item["title"],
+                $item["excerpt"],
+                $item["content"],
+                $author,
+                new \DateTimeImmutable($item["created_at"])
+            );
+            $post->setId((int)$item["id"]);
+            $posts[] = $post;
+        }
+
+        return $posts;
+    }
+
+    public function findOne(int $id): ?Post {
+        $query = $this->db->prepare('SELECT * FROM posts WHERE id = :id');
+        $query->execute(['id' => $id]);
+        $item = $query->fetch(PDO::FETCH_ASSOC);
+
+        if ($item) {
+            $author = $this->userManager->findOne((int)$item["author_id"]);
+            $post = new Post(
+                $item["title"],
+                $item["excerpt"],
+                $item["content"],
+                $author,
+                new \DateTimeImmutable($item["created_at"])
+            );
+            $post->setId((int)$item["id"]);
+            return $post;
+        }
+
+        return null;
+    }
+
+    public function findByCategory(int $categoryId): array {
+        $query = $this->db->prepare('SELECT * FROM posts WHERE category_id = :category_id ORDER BY created_at DESC');
+        $query->execute(['category_id' => $categoryId]);
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        $posts = [];
+
+        foreach ($result as $item) {
+            $author = $this->userManager->findOne((int)$item["author_id"]);
+            $post = new Post(
+                $item["title"],
+                $item["excerpt"],
+                $item["content"],
+                $author,
+                new \DateTimeImmutable($item["created_at"])
+            );
+            $post->setId((int)$item["id"]);
+            $posts[] = $post;
+        }
+
+        return $posts;
+    }
 }
+
+?>
